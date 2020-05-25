7486b7c2b478aa164fb66d2b7fcf4a558f38e9a1
ajaivasudeve <ajaivasudeve@gmail.com>
2020-05-25 19:56:54 +0530

    msm_performance: prevent userspace from setting min/max cpufreq
    Signed-off-by: ajaivasudeve <ajaivasudeve@gmail.com>

--- drivers/soc/qcom/msm_performance.c
+++ drivers/soc/qcom/msm_performance.c
@@ -404,6 +404,8 @@ static int set_cpu_min_freq(const char *buf, const struct kernel_param *kp)
 	cpumask_var_t limit_mask;
 	int ret;
 
+	return 0;
+
 	while ((cp = strpbrk(cp + 1, " :")))
 		ntokens++;
 
@@ -486,6 +488,8 @@ static int set_cpu_max_freq(const char *buf, const struct kernel_param *kp)
 	struct cpufreq_policy policy;
 	cpumask_var_t limit_mask;
 	int ret;
+
+	return 0;
 
 	while ((cp = strpbrk(cp + 1, " :")))
 		ntokens++;
